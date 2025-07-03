0a25b6e8f1e763df64950dcc3bf1bd71ddaee195
FordenRo <FordenRo@yandex.ru>
2025-07-03 20:47:21 +0500

    Stoneface anim

---
+++ handlers/stoneface_anim.py
@@ -0,0 +1,25 @@
+from asyncio import sleep
+
+from telethon.errors import MessageIdInvalidError
+from telethon.events import NewMessage
+from telethon.tl.custom import Message
+
+from client import client
+
+lifetime = 60
+
+
+@client.on(NewMessage(outgoing=True))
+async def command(message: Message):
+    if '-_-' not in message.text:
+        return
+
+    for i in range(int(lifetime / 5)):
+        try:
+            text = message.text
+            await message.edit(text.replace('-_-', '=_='))
+            await sleep(1)
+            await message.edit(text)
+            await sleep(4)
+        except MessageIdInvalidError:
+            return
