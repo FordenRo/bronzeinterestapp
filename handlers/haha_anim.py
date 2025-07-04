2df232fb33fa18e744cc9de9028cfab1dc30a25a
FordenRo <FordenRo@yandex.ru>
2025-07-04 18:43:14 +0500

    Haha anim

---
+++ handlers/haha_anim.py
@@ -0,0 +1,31 @@
+import re
+from asyncio import sleep
+
+from telethon.errors import MessageIdInvalidError
+from telethon.events import NewMessage
+from telethon.tl.custom import Message
+
+from client import client
+from handlers.read_handler import register_on_read_event
+
+lifetime = 10
+
+
+@client.on(NewMessage(outgoing=True))
+async def command(message: Message):
+    if not re.search('(?=ха|ах)[ха]{5}', message.text, re.IGNORECASE):
+        return
+
+    register_on_read_event(message, anim)
+
+
+async def anim(message: Message):
+    part = re.search('(?=[ха]{5})(?=ха|ах)\\w+', message.text, re.IGNORECASE).group()
+    for i in range(len(part) * (2 if len(part) < 15 else 1)):
+        try:
+            await sleep(0.4)
+            p = i % len(part) + 1
+            text = part[p:] + part[:p]
+            await message.edit(message.text.replace(part, text))
+        except MessageIdInvalidError:
+            return
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from client import client
 # noinspection PyUnresolvedReferences
 from handlers import (repeat_handler, love_emoji_anim, love_show,
                       love_word_anim, magic_anim, stoneface_anim,
-                      triple_anim, want_anim)
+                      triple_anim, want_anim, haha_anim)
 
 
 async def main():
