5d910c674eaddff577580328a0988df754bd6602
FordenRo <FordenRo@yandex.ru>
2025-07-04 18:01:46 +0500

    More love emojis

--- handlers/love_emoji_anim.py
+++ handlers/love_emoji_anim.py
@@ -8,6 +8,7 @@ from client import client
 from handlers.read_handler import register_on_read_event
 
 lifetime = 60
+emojis = ['💜️', '🩷', '💛', '💙', '💚']
 
 
 @client.on(NewMessage(outgoing=True))
@@ -19,12 +20,13 @@ async def command(message: Message):
 
 
 async def anim(message: Message):
-    for i in range(int(lifetime / 6)):
+    for i in range(int(lifetime / (3 + len(emojis) * 3))):
         try:
             text = message.text
             await sleep(3)
-            await message.edit(text.replace('❤️', '💜️'))
-            await sleep(3)
+            for i in emojis:
+                await message.edit(text.replace('❤️', i))
+                await sleep(3)
             await message.edit(text)
         except MessageIdInvalidError:
             return
--- handlers/read_handler.py
+++ handlers/read_handler.py
@@ -9,7 +9,7 @@ from client import client
 def register_on_read_event(message: Message, func: Callable[[Message], Coroutine]):
     @client.on(MessageRead([message.chat_id]))
     async def on_read(event):
-        if not event.is_read(message.id):
+        if event.max_id < message.id:
             return
 
         client.loop.create_task(func(message))
