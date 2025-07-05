8b0d4e1493a1a1eab238b31870d2bb14c22eb330
FordenRo <FordenRo@yandex.ru>
2025-07-05 14:09:05 +0500

    Added system commands

---
+++ bot_handlers/system_commands.py
@@ -0,0 +1,37 @@
+import sys
+from asyncio import create_subprocess_shell
+from asyncio.subprocess import PIPE
+
+from telethon.events import NewMessage
+from telethon.tl.custom import Message
+
+from client import bot, client
+
+
+@bot.on(NewMessage(incoming=True, pattern='stop'))
+async def stop(message: Message):
+    await message.respond('Stopping...')
+
+    client.loop.stop()
+    sys.exit()
+
+
+@bot.on(NewMessage(incoming=True, pattern='restart'))
+async def restart(message: Message):
+    await message.respond('Restarting...')
+
+    await create_subprocess_shell(f'"{sys.executable}" {' '.join(sys.argv)}')
+    client.loop.stop()
+    sys.exit()
+
+
+@bot.on(NewMessage(incoming=True, pattern='update'))
+async def update(message: Message):
+    cmd = await create_subprocess_shell('git pull', stdout=PIPE)
+    stdout, _ = await cmd.communicate()
+    output = stdout.decode('utf8')
+
+    await message.respond(f'<pre>{output}</pre>')
+
+    if output != 'Already up to date.':
+        await restart(message)
