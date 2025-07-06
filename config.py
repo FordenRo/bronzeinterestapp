99843185abd68153f2c7a37bba9147dc59e19170
FordenRo <FordenRo@yandex.ru>
2025-07-06 08:04:35 +0500

    Дополнена HTML-разметка справочных сообщений

--- config.py
+++ config.py
@@ -3,33 +3,33 @@ import json
 
 
 help_messages = {
-    'online': 'be|no online - <i>включить|выключить постоянный онлайн</i>\n'
-    'online state|status - <i>проверить статус постоянного онлайна</i>\n'
-    'online help - <i>справка по онлайну</i>',
+    'online': '<code>be|no online</code> - <i>включить|выключить постоянный онлайн</i>\n'
+    '<code>online state|status</code> - <i>проверить статус постоянного онлайна</i>\n'
+    '<code>online help</code> - <i>справка по онлайну</i>',
 
-    'spy': 'spy online|read @username onetime? - <i>следить за онлайном|чтением пользователя</i>\n'
-           'spy remove online|read @username - <i>убрать из слежения</i>\n'
-           'spy list - <i>список пользователей в слежении</i>\n'
-           'spy help - <i>справка по слежению</i>',
+    'spy': '<code>spy online|read @username onetime?</code> - <i>следить за онлайном|чтением пользователя</i>\n'
+           '<code>spy remove online|read @username</code> - <i>убрать из слежения</i>\n'
+           '<code>spy list</code> - <i>список пользователей в слежении</i>\n'
+           '<code>spy help</code> - <i>справка по слежению</i>',
 
-    'autoresponder': 'autoresponder @nickname silent? text - <i>настроить автоответ</i>\n'
-                     'autoresponder remove @nickname - <i>убрать автоответ</i>\n'
-                     'autoresponder list - <i>список пользователей в автоответчике</i>\n'
-                     'autoresponder help - <i>справка по автоответчику</i>',
+    'autoresponder': '<code>autoresponder @nickname silent? text</code> - <i>настроить автоответ</i>\n'
+                     '<code>autoresponder remove @nickname</code> - <i>убрать автоответ</i>\n'
+                     '<code>autoresponder list</code> - <i>список пользователей в автоответчике</i>\n'
+                     '<code>autoresponder help</code> - <i>справка по автоответчику</i>',
 
-    'autoreader': 'autoreader @nickname silent? onetime? - <i>автоматически читать сообщения</i>\n'
-                  'autoreader remove @nickname - <i>убрать авточитатель</i>\n'
-                  'autoreader list - <i>список пользователей в авточитателе</i>\n'
-                  'autoreader help - <i>справка по авточитателю</i>',
+    'autoreader': '<code>autoreader @nickname silent? onetime?</code> - <i>автоматически читать сообщения</i>\n'
+                  '<code>autoreader remove @nickname</code> - <i>убрать авточитатель</i>\n'
+                  '<code>autoreader list</code> - <i>список пользователей в авточитателе</i>\n'
+                  '<code>autoreader help</code> - <i>справка по авточитателю</i>',
 
-    'log': 'log send - <i>отправить файл с логом</i>\n'
-           'log clear|cls - <i>очистить лог</i>\n'
-           'log help - <i>справка по логам</i>',
+    'log': '<code>log send</code> - <i>отправить файл с логом</i>\n'
+           '<code>log clear|cls</code> - <i>очистить лог</i>\n'
+           '<code>log help</code> - <i>справка по логам</i>',
 
-    'system': 'stop - <i>остановить бота</i>\n'
-              'restart - <i>перезапустить бота</i>\n'
-              'update - <i>обновить и перезапустить</i>\n'
-              'update check - <i>проверить доступные обновления</i>',
+    'system': '<code>stop</code> - <i>остановить бота</i>\n'
+              '<code>restart</code> - <i>перезапустить бота</i>\n'
+              '<code>update</code> - <i>обновить и перезапустить</i>\n'
+              '<code>update check</code> - <i>проверить доступные обновления</i>',
 }
 
 if not os.path.exists('config.json'):
